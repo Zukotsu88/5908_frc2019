112e8ab2f9c2231d8854c757e2b95e4a178d87f4
Amir Razvi <zukotsu88@gmail.com>
2019-01-19 11:22:51 -0600

    first project template

---
+++ src/main/java/frc/robot/OI.java
@@ -0,0 +1,50 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.buttons.Trigger;
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+/* Default Values */
+  private static final double DEFAULT_THUMBSTICK_DEADZONE = 0.1;  // Jiggle room for the thumbsticks
+  private static final double DEFAULT_TRIGGER_DEADZONE    = 0.01; // Jiggle room for the triggers
+  private static final double DEFAULT_TRIGGER_SENSITIVITY = 0.6;  // If the trigger is beyond this limit, say it has been pressed
+  
+  /* Button Mappings */
+  private static final int    A_BUTTON_ID                 = 1;
+  private static final int    B_BUTTON_ID                 = 2;
+  private static final int    X_BUTTON_ID                 = 3;
+  private static final int    Y_BUTTON_ID                 = 4;
+  private static final int    LB_BUTTON_ID                = 5;
+  private static final int    RB_BUTTON_ID                = 6;
+  private static final int    BACK_BUTTON_ID              = 7;
+  private static final int    START_BUTTON_ID             = 8;
+  private static final int    LEFT_THUMBSTIKC_BUTTON_ID   = 9;
+  private static final int    RIGHT_THUMBSTICK_BUTTON_ID  = 10;
+  
+  /* Axis Mappings */
+  private static final int    LEFT_THUMBSTICK_X_AXIS_ID   = 0;
+  private static final int    LEFT_THUMBSTICK_Y_AXIS_ID   = 1;
+  private static final int    LEFT_TRIGGER_AXIS_ID        = 2;
+  private static final int    RIGHT_TRIGGER_AXIS_ID       = 3;
+  private static final int    RIGHT_THUMBSTICK_X_AXIS_ID  = 4;
+  private static final int    RIGHT_THUMBSTICK_Y_AXIS_ID  = 5;  
+
+
+Joystick xbox0 = new Joystick(0);
+Joystick xbox1 = new Joystick(1);
+
+
+}
+
---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,57 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.Solenoid;
+/*import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.PowerDistributionPanel;
+import edu.wpi.first.wpilibj.interfaces.Gyro;*/
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+  // For example to map the left and right motors, you could define the
+  // following variables to use with your drivetrain subsystem.
+  // public static int leftMotor = 1;
+  // public static int rightMotor = 2;
+  // If you are using multiple modules, make sure to define both the port
+  // number and the module. For example you with a rangefinder:
+  // public static int rangefinderPort = 1;
+  // public static int rangefinderModule = 1;
+
+
+  public static final class DriveMap {
+    public static final TalonSRX rightMotor1 = new TalonSRX(15);
+    public static final TalonSRX rightMotor2 = new TalonSRX(14);
+    public static final TalonSRX rightMotor3 = new TalonSRX(13);
+
+    public static final TalonSRX leftMotor1 = new TalonSRX(0);
+    public static final TalonSRX leftMotor2 = new TalonSRX(1);
+    public static final TalonSRX leftMotor3 = new TalonSRX(2);
+
+    public static final Solenoid climbPTO = new Solenoid(0);
+    public static final Solenoid shiftPTO = new Solenoid(5);
+  }
+
+
+
+
+
+
+
+
+
+}
+
+
